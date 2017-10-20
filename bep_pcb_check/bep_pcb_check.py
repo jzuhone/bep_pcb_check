6c1f0393bf744f72da27d869146903420115ca37
John ZuHone <jzuhone@gmail.com>
2017-10-20 17:33:11 -0400

    Bring this up to speed with the latest version of acis_thermal_check

--- bep_pcb_check/bep_pcb_check.py
+++ bep_pcb_check/bep_pcb_check.py
@@ -6,9 +6,8 @@ bep_pcb_check
 ========================
 
 This code generates backstop load review outputs for checking the ACIS
-DPA temperature 1DPAMZT.  It also generates DPA model validation
-plots comparing predicted values to telemetry for the previous three
-weeks.
+BEP PCB temperature. It also generates BEP PCB model validation plots
+comparing predicted values to telemetry for the previous three weeks.
 """
 from __future__ import print_function
 
@@ -20,38 +19,21 @@ matplotlib.use('Agg')
 import numpy as np
 import xija
 import sys
-from acis_thermal_check.main import ACISThermalCheck
-from acis_thermal_check.utils import calc_off_nom_rolls, get_options
+from acis_thermal_check import \
+    ACISThermalCheck, \
+    calc_off_nom_rolls, \
+    get_options, \
+    state_builders
 import os
 
 model_path = os.path.abspath(os.path.dirname(__file__))
 
-MSID = dict(bep_pcb='TMP_BEP_PCB')
-# This is the Yellow High IPCL limit.
-# 05/2014 - changed from 35.0 to 37.5
-YELLOW = dict(bep_pcb=44.0)
-# This is the difference between the Yellow High IPCL limit and 
-# the Planning Limit. So the Planning Limit is YELLOW - MARGIN
-#
-# 12/5/13 - This value was changed from 2.5 to 2.0 to reflect the new 
-# 1DPAMZT planning limit of 33 degrees C
-# 05/19/14 this is changed from 2.0, to 3.0.  2 degress for the normal
-#          padding for model error and an additional degree because
-#          the total change is being done in increments. We will back
-#          this off from 3 degrees to two after a few months trial 
-#          testing.  So for now the planning limit will be 34.5 deg. C.
-# 09/19/14 - Set MARGIN to 2.0 so that the Planning Limit is now 
-#            35.5 deg. C
-MARGIN = dict(bep_pcb=2.0)
-# 12/5/13 - Likewise the 1DPAMZT validation limits were reduced to 2.0 
-#           from 2.5 for the 1% and 99% quantiles
-VALIDATION_LIMITS = {'TMP_BEP_PCB': [(1, 2.0),
-                                       (50, 1.0),
-                                       (99, 2.0)],
-                     'PITCH': [(1, 3.0),
-                               (99, 3.0)],
-                     'TSCPOS': [(1, 2.5),
-                                (99, 2.5)]
+MSID = {"bep_pcb": 'TMP_BEP_PCB'}
+YELLOW = {"bep_pcb": 44.0}
+MARGIN = {"bep_pcb": 2.0}
+VALIDATION_LIMITS = {'TMP_BEP_PCB': [(1, 2.0), (50, 1.0), (99, 2.0)],
+                     'PITCH': [(1, 3.0), (99, 3.0)],
+                     'TSCPOS': [(1, 2.5), (99, 2.5)]
                      }
 HIST_LIMIT = [20.]
 
@@ -70,12 +52,12 @@ def calc_model(model_spec, states, start, stop, T_bep=None, T_bep_times=None):
     model.calc()
     return model
 
-bep_pcb_check = ACISThermalCheck("tmp_bep_pcb", "bep_pcb", MSID,
-                                  YELLOW, MARGIN, VALIDATION_LIMITS,
-                                  HIST_LIMIT, calc_model)
-
 def main():
-    args = get_options("TMP_BEP_PCB", "bep_pcb", model_path)
+    args = get_options("bep_pcb", model_path)
+    bep_pcb_check = ACISThermalCheck("tmp_bep_pcb", "bep_pcb",
+                                     state_builders[args.state_builder], MSID,
+                                     YELLOW, MARGIN, VALIDATION_LIMITS,
+                                     HIST_LIMIT, calc_model)
     try:
         bep_pcb_check.driver(args)
     except Exception as msg:
